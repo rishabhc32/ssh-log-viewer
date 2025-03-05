0e991759d10988f85a7fe9a96b7bc639ffd8d63f
Rishabh Chauhan <rishabhc32@gmail.com>
2025-03-05 13:40:26 +0530

    Changes

--- ssh-log-viewer/Utils/SSHManager.swift
+++ ssh-log-viewer/Utils/SSHManager.swift
@@ -1,5 +1,16 @@
 import mft
 
 class SSHManager {
-    
+    func connect() throws {
+        let sftp = MFTSftpConnection(
+            hostname: "123.123.123.123",
+            port: 22,
+            username: "your_user_name",
+            password: "your_secret_password"
+        )
+        
+        try sftp.connect()
+        try sftp.authenticate()
+    }
 }
+
